d9e6dbb56f3bf09f3462bb5fff9161052c199b01
emilycristine <emilycristhine@hotmail.com>
2022-01-12 18:40:19 -0300

    primeiro envio

--- katas3.js
+++ katas3.js
@@ -1,76 +1,188 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 function kata1() {
-    // implemente o código do kata 1 aqui
+    let retorno = [];
+    for (let n = 1; n <= 25; n++) {
+        retorno.push(n)
+    }
+    return retorno;
 }
+console.log(kata1());
+
 
 function kata2() {
-    // implemente o código do kata 2 aqui
+    let retorno = [];
+    for (let n = 25; n >= 1; n--) {
+        retorno.push(n)
+    }
+    return retorno;
 }
+console.log(kata2());
+
 
 function kata3() {
-    // implemente o código do kata 3 aqui
+    let retorno = [];
+    for (let n = -1; n >= -25; n--) {
+        retorno.push(n)
+    }
+    return retorno;
 }
+console.log(kata3());
+
 
 function kata4() {
-    // implemente o código do kata 4 aqui
+    let retorno = [];
+    for (let n = -25; n <= -1; n++) {
+        retorno.push(n)
+    }
+    return retorno;
 }
+console.log(kata4());
+
 
 function kata5() {
-    // implemente o código do kata 5 aqui
+    let retorno = [];
+    for (let n = 25; n >= -25; n -= 2) {
+        retorno.push(n)
+    }
+    return retorno;
 }
+console.log(kata5());
+
 
 function kata6() {
-    // implemente o código do kata 6 aqui
+    let retorno = [];
+    for (let n = 3; n <= 100; n += 3) {
+        retorno.push(n)
+    }
+    return retorno;
 }
+console.log(kata6());
+
 
 function kata7() {
-    // implemente o código do kata 7 aqui
+    let retorno = [];
+    for (let n = 7; n <= 100; n += 7) {
+        retorno.push(n)
+    }
+    return retorno;
 }
+console.log(kata7());
+
 
 function kata8() {
-    // implemente o código do kata 8 aqui
+    let retorno = [];
+    for (let n = 100; n >= 1; n--) {
+        if (n % 7 === 0 || n % 3 === 0) {
+            retorno.push(n)
+        }
+    }
+    return retorno;
 }
+console.log(kata8());
+
 
 function kata9() {
-    // implemente o código do kata 9 aqui
+    let retorno = [];
+    for (let n = 1; n <= 100; n++) {
+        if (n % 5 === 0 && n % 2 !== 0) {
+            retorno.push(n)
+        }
+    }
+    return retorno;
 }
+console.log(kata9());
+
 
 function kata10() {
-    // implemente o código do kata 10 aqui
+    return sampleArray;
 }
+console.log(kata10());
+
 
 function kata11() {
-    // implemente o código do kata 11 aqui
+    let retorno = [];
+    for (let n = 0; n < sampleArray.length; n++) {
+        if (sampleArray[n] % 2 === 0) {
+            retorno.push(sampleArray[n])
+        }
+    }
+    return retorno;
 }
+console.log(kata11());
+
 
 function kata12() {
-    // implemente o código do kata 12 aqui
+    let retorno = [];
+    for (let n = 0; n <= sampleArray.length; n++) {
+        if (sampleArray[n] % 2 === 1) {
+            retorno.push(sampleArray[n])
+        }
+    }
+    return retorno;
 }
+console.log(kata12());
 
 function kata13() {
-    // implemente o código do kata 13 aqui
+    let retorno = [];
+    for (let n = 0; n < sampleArray.length; n++) {
+        if (sampleArray[n] % 8 === 0) {
+            retorno.push(sampleArray[n])
+        }
+    }
+    return retorno;
 }
+console.log(kata13());
+
 
 function kata14() {
-    // implemente o código do kata 14 aqui
+    let retorno = [];
+    for (let n = 0; n < sampleArray.length; n++) {
+        retorno.push(sampleArray[n] * sampleArray[n])
+    }
+    return retorno;
 }
+console.log(kata14());
 
 function kata15() {
-    // implemente o código do kata 15 aqui
+    let retorno = 0;
+    for (let n = 1; n <= 20; n++) {
+        retorno += n
+    }
+    return retorno;
 }
+console.log(kata15());
 
 function kata16() {
-    // implemente o código do kata 16 aqui
+    let retorno = 0;
+    for (let n in sampleArray) {
+        retorno += sampleArray[n]
+    }
+    return retorno;
 }
+console.log(kata16());
 
 function kata17() {
-    // implemente o código do kata 17 aqui
+    let retorno = sampleArray[0];
+    for (let n = 0; n < sampleArray.length; n++) {
+        if (sampleArray[n] < retorno) {
+            retorno = sampleArray[n]
+        }
+    }
+    return retorno;
 }
+console.log(kata17());
 
 function kata18() {
-    // implemente o código do kata 18 aqui
+    let retorno = sampleArray[0];
+    for (let n = 0; n < sampleArray.length; n++) {
+        if (sampleArray[n] > retorno) {
+            retorno = sampleArray[n]
+        }
+    }
+    return retorno;
 }
+console.log(kata18());
 
 /**
  * Daqui em diante são os bônus, por sua conta e risco
